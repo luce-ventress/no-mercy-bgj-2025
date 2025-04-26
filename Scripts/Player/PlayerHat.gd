04a23b272dde16ab91a5d97e47747505a0c8b3b7
Lucie Venhoda <lucie@venhoda.com>
2025-04-26 12:21:52 +0200

    Set up camera angle, distance; Polished player hat code and looks

--- Scripts/Player/PlayerHat.gd
+++ Scripts/Player/PlayerHat.gd
@@ -1,11 +1,18 @@
 extends Node3D
+
+@export var hat_speed: float = 1
+@export var hat_magnitude: float = 0.5
+
+var magnitude_scale: float = 100
+
 @onready var time: float = 0
+
 func _process(delta: float) -> void:
 	
 	time += delta
-	var move = sin((2 * PI * time))
-	
-	var deltaPos = Vector3(0, move/200, 0)
+	var sinValue = sin((2 * PI * time * hat_speed))
+	print()
+	var deltaPos = transform.basis.y * sinValue * (hat_magnitude / magnitude_scale)
 	var meshPos  = get_position()
 	set_position(meshPos + deltaPos)
 	
