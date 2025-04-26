deb00de0d802bf240bb9b88b6e907f0bc3d756fe
Nikth42 <126991618+Nikth42@users.noreply.github.com>
2025-04-26 11:30:46 +0200

    added hat movement

--- Scripts/Player/PlayerHat.gd
+++ Scripts/Player/PlayerHat.gd
@@ -1,1 +1,11 @@
-extends Node
+extends Node3D
+@onready var time: float = 0
+func _process(delta: float) -> void:
+	
+	time += delta
+	var move = sin((2 * PI * time))
+	
+	var deltaPos = Vector3(0, move/200, 0)
+	var meshPos  = get_position()
+	set_position(meshPos + deltaPos)
+	
