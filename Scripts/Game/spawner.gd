4c409ef363c253994bdfe9240585f4110286dbe3
Nikth42 <126991618+Nikth42@users.noreply.github.com>
2025-04-27 12:52:29 +0200

    spawner stops after player death

--- Scripts/Game/spawner.gd
+++ Scripts/Game/spawner.gd
@@ -12,6 +12,9 @@ func getSpawnLocation() -> Vector3:
 	return spawnLocation
 
 func _on_wave_timer_timeout() -> void:
+	if $PlayerPawn.isDead:
+		return
+		
 	var MeleeEnemy = meleeEnemyScene.instantiate()
 	owner.add_child(MeleeEnemy)
 	MeleeEnemy.set_position(getSpawnLocation())
