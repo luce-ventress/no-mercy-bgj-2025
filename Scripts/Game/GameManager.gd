8df6777179543581ad305250a88a2d6de1c8770a
Nikth42 <126991618+Nikth42@users.noreply.github.com>
2025-04-27 15:06:21 +0200

    stop timer

--- Scripts/Game/GameManager.gd
+++ Scripts/Game/GameManager.gd
@@ -9,3 +9,11 @@ func _process(delta: float) -> void:
 	time = Time.get_unix_time_from_system() - game_start_time
 	var time_string = Time.get_time_string_from_unix_time(time)
 	game_time_label.text = time_string
+	if PlayerStats.isDead:
+		showEndTime()
+	
+func showEndTime() ->void:
+	time = Time.get_unix_time_from_system() - game_start_time
+	var endTime = Time.get_time_string_from_unix_time(time)
+	game_time_label.text = "End time: " + endTime
+	set_process(!is_processing())
